9efa12190f20aba024a9a83da6a5b91867639ccb
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-11-28 15:19:00 -0800

    fix config
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- run.cmd
+++ run.cmd
@@ -3,4 +3,4 @@ docker rm fabric.smtp.agent
 
 docker build -t healthcatalyst/fabric.smtp.agent . 
 
-docker run --rm --name fabric.smtp.agent -t healthcatalyst/fabric.smtp.agent
+docker run -p 25:25 --rm --name fabric.smtp.agent -t healthcatalyst/fabric.smtp.agent
